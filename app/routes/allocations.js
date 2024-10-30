8a7a5a5605e1f79242f2f2e03443f0467ac7f175
Mohamed AboElKheir <mohamed.aboelkheir@ironcladhq.com>
2024-10-30 14:37:47 -0400

    clone of NodeGoat but without the comments

---
+++ app/routes/allocations.js
@@ -0,0 +1,30 @@
+const AllocationsDAO = require("../data/allocations-dao").AllocationsDAO;
+const {
+    environmentalScripts
+} = require("../../config/config");
+
+function AllocationsHandler(db) {
+    "use strict";
+
+    const allocationsDAO = new AllocationsDAO(db);
+
+    this.displayAllocations = (req, res, next) => {
+        const {
+            userId
+        } = req.params;
+        const {
+            threshold
+        } = req.query;
+
+        allocationsDAO.getByUserIdAndThreshold(userId, threshold, (err, allocations) => {
+            if (err) return next(err);
+            return res.render("allocations", {
+                userId,
+                allocations,
+                environmentalScripts
+            });
+        });
+    };
+}
+
+module.exports = AllocationsHandler;
---
+++ app/routes/profile.js
@@ -0,0 +1,97 @@
+const ProfileDAO = require("../data/profile-dao").ProfileDAO;
+const ESAPI = require("node-esapi");
+const {
+    environmentalScripts
+} = require("../../config/config");
+
+/* The ProfileHandler must be constructed with a connected db */
+function ProfileHandler(db) {
+    "use strict";
+
+    const profile = new ProfileDAO(db);
+
+    this.displayProfile = (req, res, next) => {
+        const {
+            userId
+        } = req.session;
+
+
+
+        profile.getByUserId(parseInt(userId), (err, doc) => {
+            if (err) return next(err);
+            doc.userId = userId;
+
+            doc.website = ESAPI.encoder().encodeForHTML(doc.website);
+
+            return res.render("profile", {
+                ...doc,
+                environmentalScripts
+            });
+        });
+    };
+
+    this.handleProfileUpdate = (req, res, next) => {
+
+        const {
+            firstName,
+            lastName,
+            ssn,
+            dob,
+            address,
+            bankAcc,
+            bankRouting
+        } = req.body;
+
+        const regexPattern = /([0-9]+)+\#/;
+        // Allow only numbers with a suffix of the letter #, for example: 'XXXXXX#'
+        const testComplyWithRequirements = regexPattern.test(bankRouting);
+        // if the regex test fails we do not allow saving
+        if (testComplyWithRequirements !== true) {
+            const firstNameSafeString = firstName;
+            return res.render("profile", {
+                updateError: "Bank Routing number does not comply with requirements for format specified",
+                firstNameSafeString,
+                lastName,
+                ssn,
+                dob,
+                address,
+                bankAcc,
+                bankRouting,
+                environmentalScripts
+            });
+        }
+
+        const {
+            userId
+        } = req.session;
+
+        profile.updateUser(
+            parseInt(userId),
+            firstName,
+            lastName,
+            ssn,
+            dob,
+            address,
+            bankAcc,
+            bankRouting,
+            (err, user) => {
+
+                if (err) return next(err);
+
+                // WARN: Applying any sting specific methods here w/o checking type of inputs could lead to DoS by HPP
+                //firstName = firstName.trim();
+                user.updateSuccess = true;
+                user.userId = userId;
+
+                return res.render("profile", {
+                    ...user,
+                    environmentalScripts
+                });
+            }
+        );
+
+    };
+
+}
+
+module.exports = ProfileHandler;
