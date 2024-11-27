fae0af051490deef03d840068ce8fda93ceb94f7
Mohamed AboElKheir <mohamed.aboelkheir@ironcladhq.com>
2024-11-27 15:15:57 -0500

    Update contributions.js

--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -33,12 +33,6 @@ function ContributionsHandler(db) {
         const afterTax = eval(req.body.afterTax);
         const roth = eval(req.body.roth);
 
-        /*
-        //Fix for A1 -1 SSJS Injection attacks - uses alternate method to eval
-        const preTax = parseInt(req.body.preTax);
-        const afterTax = parseInt(req.body.afterTax);
-        const roth = parseInt(req.body.roth);
-        */
         const {
             userId
         } = req.session;
